efb93660829c4c652e7c792af78de560637e96ec
lizzyblue123 <lizzyblue1999@gmail.com>
2015-09-14 08:03:36 -0400

    Madlib

---
+++ Madlib.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Madlib
+   public static void main(String args[]);
+   Scanner liz = new Scanner(System.in);
+  
+   String wordRelative = "";
+   String wordAdjective = "";
+   String wordAdjective1 = "";
+   String wordAdjective2 = "";
+   String wordNameOfPersonInRoom = "";
+   String wordAdjective3 = "";
+   String wordAdjective4 = "";
+   String wordVerbEndingInED = "";
+   String wordBodyPart = "";
+   String wordVerbEndingInING = "";
+   String wordNounPlural = "";
+   String wordNoun = "":
+   String wordAdverb = "";
+   String wordVerb = "";
+   String wordVerb2 = "";
+   String wordRelative2 = "";
+   String wordPersonInRoom = "";
+   
+   System.out.println("Enter a kind of Relative.");
+   wordRelative = scnr.nextLine();
+   
+   System.out.println("Enter an Adjective.");
+   wordAdjective = scnr.nextLine();
+   
+   System.out.println("Enter another Adjective.");
+   wordAdjective1 = scnr.nextLine();
+   
+   System.out.println("Enter a different Adjective.");
+   wordAdjective2 = scnr.nextLine();
+   
+   System.out.println("Enter the name of a person in the room.");
+   wordNameOfPersonInRoom = scnr.nextLine();
+   
+   System.out.println("Enter an Adjective.");
+   wordAdjective3 = scnr.nextLine();
+   
+   System.out.println("Enter another Adjective.");
+   wordAdjective4 = scnr.nextLine();
+   
+   System.out.println("Enter a Verb ending in ED.");
+   wordVerbEndingInED = scnr.nextLine();
+   
+   System.out.println("Enter a Body Part.");
+   wordBodyPart = scnr.nextLine();
+   
+   
+
