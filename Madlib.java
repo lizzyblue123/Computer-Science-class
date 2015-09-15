5601cc6fae0ba453339bfd1c722bac44805a58ae
lizzyblue123 <lizzyblue1999@gmail.com>
2015-09-15 08:01:59 -0400

    Madlib1

--- Madlib.java
+++ Madlib.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
-public class Madlib
-   public static void main(String args[]);
+public class Madlib{
+   public static void main(String args[]){
+   
    Scanner liz = new Scanner(System.in);
   
    String wordRelative = "";
