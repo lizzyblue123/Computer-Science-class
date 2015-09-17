7242e7510df234abb7e0701e00277f3a04a9f2bc
lizzyblue123 <lizzyblue1999@gmail.com>
2015-09-17 10:06:27 -0400

    time lab

---
+++ timeLab.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class timeLab{
+
+public static void main(String args[]){
+
+int secondsBeforeConversion = 0;
+int min = 0;
+int hour = 0;
+int sec = 0;
+
+Scanner scnr = new Scanner(System.in);
+
+System.out.println("Enter an amount of seconds: ");
+secondsBeforeConversion = scnr.nextInt();
+
+min = secondsBeforeConversion / 60;
+sec = sec % 60;
+hour = min / 60;
+min = min % 60;
+
+System.out.println(secondsBeforeConversion + " seconds is equal to " + hour + " hour(s), " + min + " minute(s), and " + sec + " second(s).");
+
+   }
+}
