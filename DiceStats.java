5962888d586f9864871cdcb620117d8e901e7b73
lizzyblue123 <lizzyblue1999@gmail.com>
2015-11-10 07:47:58 -0500

    dice stats change

--- DiceStats.java
+++ DiceStats.java
@@ -56,81 +56,13 @@ public class DiceStats {
      }
       System.out.print("2:  ");
       
-      for (i = 0; i < numAdded[2]; i++){
+      for (i = 2; i < NUM_ELEMENTS; i++){
+         for (i = 0; i < numAdded[i]; i++){
          System.out.print("*");
+         }
+       System.out.println();
       }
-      System.out.println();
       
-      System.out.print("3:  ");
-      
-      for (i = 0; i < numAdded[3]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-      
-      System.out.print("4:  ");
-      
-      for (i = 0; i < numAdded[4]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-      
-       System.out.print("5:  ");
-      
-      for (i = 0; i < numAdded[5]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-      
-       System.out.print("6:  ");
-      
-      for (i = 0; i < numAdded[6]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-
-       System.out.print("7:  ");
-      
-      for (i = 0; i < numAdded[7]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-
-       System.out.print("8:  ");
-      
-      for (i = 0; i < numAdded[8]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-      
-       System.out.print("9:  ");
-      
-      for (i = 0; i < numAdded[9]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-      
-       System.out.print("10: ");
-      
-      for (i = 0; i < numAdded[10]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-      
-       System.out.print("11: ");
-      
-      for (i = 0; i < numAdded[11]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-
-       System.out.print("12: ");
-      
-      for (i = 0; i < numAdded[12]; i++){
-         System.out.print("*");
-      }
-      System.out.println();
-
 
 
    }
